e228100a92e1c8de64e73d1296652043fddb230c
D-YF <daiyf1028@gmail.com>
2023-01-08 17:53:44 -0600

    get Intersection Node

--- Code/0108.py
+++ Code/0108.py
@@ -14,8 +14,8 @@ class Solution(object):
         if head==None:
             return
         
-        fast = ListNode(next=head)
-        slow = fast
+        dummy_head = ListNode(next=head)
+        slow, fast = dummy_head, dummy_head
 
         while n>=0:
             if fast==None:
@@ -23,9 +23,52 @@ class Solution(object):
             fast = fast.next
             n -= 1
         
-        while fast.next:
+        while fast:
             fast = fast.next
             slow = slow.next
         slow.next = slow.next.next
 
-        return head
+        return dummy_head.next
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getLength(self, head):
+        cur = head
+        length = 0
+        while cur:
+            length += 1
+            cur = cur.next
+        return length
+    
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        lenA = self.getLength(headA)
+        lenB = self.getLength(headB)
+
+        curA = headA
+        curB = headB
+
+        diff = abs(lenA-lenB)
+        while diff:
+            if lenA>lenB:
+                curA = curA.next
+            else:
+                curB = curB.next
+            diff -= 1
+        while curA and curB:
+            if curA==curB:
+                return curA
+            curA = curA.next
+            curB = curB.next
+            
+        return None
+    
+
