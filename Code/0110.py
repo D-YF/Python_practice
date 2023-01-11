da3eb65e954ccf7a14e35688398fc2d52a2a3494
D-YF <daiyf1028@gmail.com>
2023-01-10 20:16:22 -0600

    sum of four numbers

--- Code/0110.py
+++ Code/0110.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # use two points -- O(n^2)
     def threeSum(self, nums):
         """
         :type nums: List[int]
@@ -29,8 +30,47 @@ class Solution(object):
 
         return result
 
-nums = [-1,0,1,2,-1,-4]
+# nums = [-1,0,1,2,-1,-4]
+# sol = Solution()
+# print(sol.threeSum(nums))
+
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        N = len(nums)
+        nums = sorted(nums)
+        ans = []
+
+        for i in range(N-3):
+            if ans and nums[i] == ans[-1][0]: continue
+
+            for j in range(i+1, N-2):
+                if ans and nums[i] == ans[-1][0] and nums[j] == ans[-1][1]: continue
+            
+                left = j+1
+                right = N-1
+                
+                while  left<right:
+                    if nums[i]+nums[j]+nums[left]+nums[right]==target:
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left<right and nums[left]==nums[left+1]:
+                            left += 1
+                        while left<right and nums[right]==nums[right-1]:
+                            right -= 1
+                    
+                    if nums[i]+nums[j]+nums[left]+nums[right] < target:
+                        left += 1
+                    else:
+                        right -= 1
+                
+        return ans
+
+nums = [1,0,-1,0,-2,2]
+target = 0
 sol = Solution()
-print(sol.threeSum(nums))
-
-
+print(sol.fourSum(nums, target))
