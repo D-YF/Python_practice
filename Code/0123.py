6fec8a552102f5b29ded5d63e3e5a623c4446527
D-YF <daiyf1028@gmail.com>
2023-01-23 23:16:26 -0600

    BST ancestor build add-up delete

--- Code/0123.py
+++ Code/0123.py
@@ -30,3 +30,89 @@ class Solution:
                 return right
         
         return postorder(root)
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        def search(root):
+            if root==None: return None
+
+            if key==root.val:
+                if root.left==None and root.right==None:
+                    return None
+                elif root.left==None and root.right:
+                    root = root.right
+                elif root.right==None and root.left:
+                    root = root.left
+                else:
+                    left = root.left
+                    root = root.right
+                    cur = root
+                    while cur.left:
+                        cur = cur.left
+                    cur.left = left
+            elif key>root.val:
+                root.right = search(root.right)
+            else:
+                root.left = search(root.left)
+            return root
+        
+        return search(root)
+    
+
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        def trim(root):
+            if root==None:
+                return None
+            
+            if low<=root.val<=high:
+                root.left = trim(root.left)
+                root.right = trim(root.right)
+            elif root.val<low:
+                root = trim(root.right)
+            elif root.val>high:
+                root = trim(root.left)
+            return root
+
+        return trim(root)
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+        def buildTree(nums):
+            N = len(nums)
+            if N==0: return None
+
+            mid = N//2
+            root = TreeNode(val=nums[mid])
+            root.left = buildTree(nums[:mid])
+            root.right = buildTree(nums[mid+1:])
+            return root
+        
+        return buildTree(nums)
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.pre = None
+
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def antiSearch(root):
+            if root==None:
+                return None
+            cur = root
+
+            cur.right = antiSearch(root.right)
+
+            if self.pre:
+                cur.val += self.pre.val
+            self.pre = cur
+
+            cur.left = antiSearch(root.left)
+            return cur
+
+        return antiSearch(root)
